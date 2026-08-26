c9d8602daf108583198d93dc252ecd1ae5deb2e2
Matheus Ferreira Lima Morbeck <matheusmorbeckk@gmail.com>
2026-08-25 23:10:31 -0300

    Update to use getter for personagem name

--- aula3_parte2/Main.java
+++ aula3_parte2/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         PersonagemEncapsuladoAt personagem1 = new PersonagemEncapsuladoAt();
 
-        System.out.println("Personagem 1: " + personagem1.nome);
+        System.out.println("Personagem 1: " + personagem1.getNome());
         personagem1.atacar();
         personagem1.atacar(20);
 
@@ -12,7 +12,7 @@ public class Main {
 
         PersonagemEncapsuladoAt personagem2 = new PersonagemEncapsuladoAt("Guerreiro", 100, 5);
 
-        System.out.println("Personagem 2: " + personagem2.nome);
+        System.out.println("Personagem 2: " + personagem2.getNome());
         personagem2.atacar();
         personagem2.atacar(50);
     }
