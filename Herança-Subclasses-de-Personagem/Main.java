a1ccdd3ee786380a92f2a97c9560ceb85ee28efe
Matheus Ferreira Lima Morbeck <matheusmorbeckk@gmail.com>
2026-09-02 20:26:04 -0300

    Adiciona o cabeçalho com a descrição do Uso de IA

--- Herança-Subclasses-de-Personagem/Main.java
+++ Herança-Subclasses-de-Personagem/Main.java
@@ -1,3 +1,17 @@
+/*
+USO IA:
+
+Modelos utilizados: Copilot e ChatGPT.
+
+Nesse projeto foi utilizada IA para consulta da sintaxe da linguagem Java,
+para pedir ajuda em problemas quando o VSCode indicava erro em alguma linha que eu escrevia
+e para traduzir todo o programa e a lógica feita em Java para Python, já que eu optei por fazer apenas em Java e pedi para a IA traduzir para Python, então apenas revisei o código e interpretei
+após isso, usei o chatGPT para tentar fazer o commit para o GitHub via Git Bash, mas acabou dando errado e fiz o commit diretamente na pasta raiz do repositório,
+então eu apenas fiz a mudança de pastas pelo próprio GitHub Web.
+
+O que você modificou ou validou manualmente: Tudo envolvendo a lógica eu produzi manualmente, o uso de IA foi apenas por sintaxe.
+*/
+
 public class Main {
     public static void main(String[] args) {
         Mago elara = new Mago("Elara", 100, 10, 200);
